4c5e7d62cf833b89b09511ec92c62a729d4a72e3
dildeolupbiten <tckutlu@gmail.com>
2024-11-25 15:35:39 +0100

    Update consts.hpp

--- include/consts.hpp
+++ include/consts.hpp
@@ -2,7 +2,6 @@
 #define CONSTS_HPP
 
 #define BLACK "\033[30m\u25A0\033[0m"
-#define WHITE "\033[37m\u25A0\033[0m"
 #define RED "\033[31m\u25A0\033[0m"
 #define BLUE "\033[34m\u25A0\033[0m"
 #define CLEAR "\033[2J"
